84c85ade9f3cd30ae90625bcc5fa71b3ec5aaf5c
PavanBandichode <pavan.bandichode@gmail.com>
2022-06-02 12:33:29 +0530

    Blog-App

---
+++ src/components/CommentBox.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { articlesURL } from "../utils/constant";
+import Comment from "./Comment";
+import withRouter from "./withRouter";
+
+class CommentBox extends React.Component {
+  state = {
+    comment: "",
+    comments: null,
+    errors: {
+      comment: "",
+    },
+  };
+  componentDidMount() {
+    this.fetchComments();
+  }
+
+  handleChange = (event) => {
+    let { name, value } = event.target;
+    let errors = { ...this.state.errors };
+    this.setState({ [name]: value, errors });
+  };
+  fetchComments = () => {
+    let slug = this.props.params.slug;
+    console.log(`fetch`);
+    fetch(articlesURL + `/${slug}/comments`)
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then(({ errors }) => {
+            return Promise.reject(errors);
+          });
+        }
+        return res.json();
+      })
+      .then(({ comments }) => {
+        console.log(comments);
+        this.setState({
+          comments,
+          comment: "",
+        });
+      })
+      .catch((errors) => console.log(errors));
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { comment } = this.state;
+    let slug = this.props.params.slug;
+    fetch(`${articlesURL}/${slug}/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Token ${this.props.user.token}`,
+      },
+      body: JSON.stringify({
+        comment: {
+          body: comment,
+        },
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`can not create new comment`);
+        }
+        return res.json();
+      })
+      .then(({ comment }) => {
+        this.setState(
+          {
+            comment: "",
+          },
+          this.fetchComments
+        );
+      })
+      .catch((errors) => {
+        this.setState({ errors });
+      });
+  };
+  deleteComment = (id) => {
+    console.log(`delete`);
+    let slug = this.props.params.slug;
+    fetch(`${articlesURL}/${slug}/comments/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Token ${this.props.user.token}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`can not delete comment`);
+        }
+        return res.json();
+      })
+      .then(({ comment }) => {
+        this.setState(
+          {
+            comment: "",
+          },
+          this.fetchComments
+        );
+      })
+      .catch((errors) => {
+        this.setState({ errors });
+      });
+  };
+  render() {
+    // let { body } = this.state.errors;
+
+    return (
+      <>
+        <section className="container">
+          <form className="comment-box">
+            <fieldset>
+              <textarea
+                onChange={this.handleChange}
+                type="text"
+                name="comment"
+                value={this.state.comment}
+                placeholder="Write a comment..."
+              />
+            </fieldset>
+            <div className="post-comment flex space-between">
+              <img src={this.props.user.image} alt={this.props.user.username} />
+              <button type="submit" onClick={this.handleSubmit}>
+                Post Comment
+              </button>
+            </div>
+          </form>
+          <div>
+            {this.state.comments &&
+              this.state.comments.map((comment, i) => (
+                <Comment
+                  key={i}
+                  {...comment}
+                  deleteComment={this.deleteComment}
+                  user={this.props.user}
+                />
+              ))}
+          </div>
+        </section>
+      </>
+    );
+  }
+}
+export default withRouter(CommentBox);
---
+++ src/components/ProfileBanner.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import withRouter from "./withRouter";
+function ProfileBanner(props) {
+  const { image, username, following } = props.profile;
+
+  return (
+    <section className="profile-banner">
+      <div className="container">
+        <div className="p-banner-box">
+          <img
+            src={
+              image ||
+              `https://static.productionready.io/images/smiley-cyrus.jpg`
+            }
+            alt="img"
+          />
+          <p> {username}</p>
+        </div>
+        <div className="flex flex-end">
+          {props.user && props.user.username === username ? (
+            <Link to="/settings">
+              <button className="logout-btn edit-profile-btn">
+                ⚙️ Edit Profile Setting
+              </button>
+            </Link>
+          ) : (
+            <div className="follow-box">
+              {following === false ? (
+                <button
+                  className="logout-btn"
+                  onClick={() => props.handleFollow(username)}
+                >
+                  + Follow {username}
+                </button>
+              ) : (
+                <button
+                  className="logout-btn"
+                  onClick={() => props.handleFollow(username)}
+                >
+                  UnFollow {username}
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default withRouter(ProfileBanner);
